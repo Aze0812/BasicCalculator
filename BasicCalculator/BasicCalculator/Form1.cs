0e1cfe44057980517d1b0c29056500c5d0cc5388
zjrncgit <140788153+Aze0812@users.noreply.github.com>
2025-10-24 16:50:08 +0800

    Basic Calculator
    A calculator that has a Private Assembly Library in C#.

---
+++ BasicCalculator/BasicCalculator/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorPrivateAssembly;
+namespace BasicCalculator
+{
+    public partial class FrmBasicCalculator : Form
+    {
+        public FrmBasicCalculator()
+        {
+            InitializeComponent();
+
+            comboBox1.Items.Add("+");
+            comboBox1.Items.Add("-");
+            comboBox1.Items.Add("*");
+            comboBox1.Items.Add("/");
+        }
+
+        private void FrmBasicCalculator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                float num1 = float.Parse(textBox1.Text);
+                float num2 = float.Parse(textBox2.Text);
+                float result = 0;
+
+                switch (comboBox1.SelectedItem.ToString())
+                {
+                    case "+":
+                        result = Calculator.Addition(num1, num2);
+                        break;
+                    case "-":
+                        result = Calculator.Subtraction(num1, num2);
+                        break;
+                    case "*":
+                        result = Calculator.Multiplication(num1, num2);
+                        break;
+                    case "/":
+                        result = Calculator.Division(num1, num2);
+                        break;
+                    default:
+                        MessageBox.Show("Please select an operation.");
+                        return;
+                }
+
+                label3.Text = result.ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter valid numbers.");
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ BasicCalculator/CalculatorPrivateAssembly/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorPrivateAssembly;
+
+namespace CalculatorPrivateAssembly
+{
+    public class Calculator
+    {
+        public static float Addition(float a, float b)
+        {
+            return a + b;
+        }
+
+        public static float Subtraction(float a, float b)
+        {
+            return a - b;
+        }
+
+        public static float Multiplication(float a, float b)
+        {
+            return a * b;
+        }
+
+        public static float Division(float a, float b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot be divided by zero.");
+            }
+            return a / b;
+        }
+    }
+}
